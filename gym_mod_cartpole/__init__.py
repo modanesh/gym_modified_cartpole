69e85ff28974921e6a1e6cec757766b822efb004
modanesh <mohamad4danesh@gmail.com>
2020-05-05 09:45:56 -0700

    slippery road added

--- gym_mod_cartpole/__init__.py
+++ gym_mod_cartpole/__init__.py
@@ -19,7 +19,7 @@ register(
     kwargs={'case':2, 'max_episode_steps':500}
 )
 register(
-    id='CartPoleMod-v2',
+    id='CartPoleMod-v3',
     entry_point='gym_mod_cartpole.envs:ModCartPoleEnv',
     kwargs={'case':3, 'max_episode_steps':500}
 )
